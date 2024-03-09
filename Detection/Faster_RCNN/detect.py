e2c06174f52303e1804a8ae53d5e27bef80488b4
gaoxam123 <minhtengi1412@gmail.com>
2024-03-10 00:19:33 +0100

    pretrained

---
+++ Detection/Faster_RCNN/detect.py
@@ -0,0 +1,26 @@
+from torchvision.io.image import read_image
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
+from torchvision.utils import draw_bounding_boxes
+from torchvision.transforms.functional import to_pil_image
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import ImageFont, ImageDraw
+
+weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.9)
+
+img = read_image("dog_bike_car.jpg")
+
+model.eval()
+preprocess = weights.transforms()
+batch = [preprocess(img)]
+prediction = model(batch)[0]
+
+labels = [weights.meta["categories"][i] for i in prediction["labels"]]
+box = draw_bounding_boxes(img, boxes=prediction["boxes"],
+                          labels=labels,
+                          colors="white",
+                          width=4, font_size=25, font='arial.ttf')
+im = to_pil_image(box.detach())
+plt.axis('off')
+plt.imshow(im)
